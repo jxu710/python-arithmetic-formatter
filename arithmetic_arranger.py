2bcb4ea28d79291e93212da58e6e94357ef3fd7e
jxu710 <101676244+jxu710@users.noreply.github.com>
2023-11-12 01:47:42 +0000

    return arranged_problems in correct format
    separate into 3 lines, each as a list, append the correct format into corresponding list and join them together

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,39 +1,44 @@
 def arithmetic_arranger(problems):
-  
   arranged_problems = ""
-  # 1st type pf error
+
+  # 1st type of error
   if len(problems) > 5:
-    return "Error: Too many problems."
+      return "Error: Too many problems."
 
-  # 2nd error, to see if operators are "+" or "-" only
+  top_lines = []
+  bottom_lines = []
+  dash_lines = []
+
   for problem in problems:
-    item = problem.split(" ")
-    # print(item)
+      item = problem.split()
 
-    operator = ["+","-"]
-    if item[1] not in operator:
-      return "Error: Operator must be '+' or '-'."
+      operator = ["+", "-"]
+      # 2nd error, to see if operators are "+" or "-" only
+      if item[1] not in operator:
+          return "Error: Operator must be '+' or '-'."
 
-    # 3rd error, to see if numbers are digits only
-    if item[0].isdigit() and item[2].isdigit() is False:
-      return "Error: Numbers must only contain digits."
+      # 3rd error, to see if numbers are digits only
+      if not item[0].isdigit() or not item[2].isdigit():
+          return "Error: Numbers must only contain digits."
 
-    # 4th error
-    if len(item[0]) > 4 or len(item[2]) > 4:
-      return "Error: Numbers cannot be more than four digits."
+      # 4th error
+      if len(item[0]) > 4 or len(item[2]) > 4:
+          return "Error: Numbers cannot be more than four digits."
 
-    # find the longest of numbers 
-    longest_number = max(len(item[0]), len(item[2]))
-    # length of the longest number + 2 for the space
-    width = (longest_number + 2)  
+      # find the longest of numbers 
+      longest_number = max(len(item[0]), len(item[2]))
+      # length of the longest number + 2 for the space
+      width = (longest_number + 2)  
 
-    # to show the operation format
+      top_lines.append(f"{item[0]:>{width}}")
+      bottom_lines.append(f"{item[1]}{item[2]:>{width-1}}")
+      dash_lines.append('-' * width)
 
-    every_problems = f"{item[0]:>{width}}\n{item[1]:>{width}}{item[2]:>{width}}\n{'-'* width}"
+  # Combine the lines to form the output
+  # takes each element in the top_lines list (which contains the top lines of each problem) and joins them together with four spaces in between each element.
+  arranged_problems += "    ".join(top_lines) + "\n"
+  arranged_problems += "    ".join(bottom_lines) + "\n"
+  arranged_problems += "    ".join(dash_lines)
 
-    # arranged_problems += every_problems
-    print(every_problems)
   return arranged_problems
 
-
-print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
